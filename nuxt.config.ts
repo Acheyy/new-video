47d168799c3364f53dee82b0fc276fa007e458c1
Bogdan <bogdan_ace_cf@yahoo.com>
2024-06-28 14:53:29 +0300

    Added latest changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,10 +7,10 @@ export default defineNuxtConfig({
       },
       script: [
         {
-          src: 'https://cdn.jsdelivr.net/npm/disable-devtool',
-          defer: true
-        }
-      ]
+          src: "https://cdn.jsdelivr.net/npm/disable-devtool",
+          defer: true,
+        },
+      ],
     },
   },
   devtools: {
@@ -26,8 +26,8 @@ export default defineNuxtConfig({
     "@nuxtjs/i18n",
     "@pinia/nuxt",
     "nuxt-icon",
-    "nuxt-simple-sitemap",
     "nuxt-swiper",
+    "@nuxtjs/sitemap",
   ],
   css: ["@/assets/css/_main.scss", "@/assets/css/toast.css"],
   site: {
@@ -35,145 +35,91 @@ export default defineNuxtConfig({
   },
   sitemap: {
     sitemaps: {
-      main : {
+      main: {
         includeAppSources: true,
       },
-      'all-girls' : {
-        sources: [
-          '/api/__sitemap__/bjList',
-        ]
+      "all-girls": {
+        sources: ["/api/__sitemap__/bjList"],
       },
-      'videos-1' : {
-        sources: [
-          '/api/__sitemap__/videos1',
-        ]
+      "videos-1": {
+        sources: ["/api/__sitemap__/videos1"],
       },
-      'videos-2' : {
-        sources: [
-          '/api/__sitemap__/videos2',
-        ]
+      "videos-2": {
+        sources: ["/api/__sitemap__/videos2"],
       },
-      'videos-3' : {
-        sources: [
-          '/api/__sitemap__/videos3',
-        ]
+      "videos-3": {
+        sources: ["/api/__sitemap__/videos3"],
       },
-      'videos-4' : {
-        sources: [
-          '/api/__sitemap__/videos4',
-        ]
+      "videos-4": {
+        sources: ["/api/__sitemap__/videos4"],
       },
-      'videos-5' : {
-        sources: [
-          '/api/__sitemap__/videos5',
-        ]
+      "videos-5": {
+        sources: ["/api/__sitemap__/videos5"],
       },
-      'videos-6' : {
-        sources: [
-          '/api/__sitemap__/videos6',
-        ]
+      "videos-6": {
+        sources: ["/api/__sitemap__/videos6"],
       },
-      'videos-7' : {
-        sources: [
-          '/api/__sitemap__/videos7',
-        ]
+      "videos-7": {
+        sources: ["/api/__sitemap__/videos7"],
       },
-      'videos-8' : {
-        sources: [
-          '/api/__sitemap__/videos8',
-        ]
+      "videos-8": {
+        sources: ["/api/__sitemap__/videos8"],
       },
-      'videos-9' : {
-        sources: [
-          '/api/__sitemap__/videos9',
-        ]
+      "videos-9": {
+        sources: ["/api/__sitemap__/videos9"],
       },
-      'videos-10' : {
-        sources: [
-          '/api/__sitemap__/videos10',
-        ]
+      "videos-10": {
+        sources: ["/api/__sitemap__/videos10"],
       },
-      'videos-11' : {
-        sources: [
-          '/api/__sitemap__/videos11',
-        ]
+      "videos-11": {
+        sources: ["/api/__sitemap__/videos11"],
       },
-      'videos-12' : {
-        sources: [
-          '/api/__sitemap__/videos12',
-        ]
+      "videos-12": {
+        sources: ["/api/__sitemap__/videos12"],
       },
-      'videos-13' : {
-        sources: [
-          '/api/__sitemap__/videos13',
-        ]
+      "videos-13": {
+        sources: ["/api/__sitemap__/videos13"],
       },
-      'videos-14' : {
-        sources: [
-          '/api/__sitemap__/videos14',
-        ]
+      "videos-14": {
+        sources: ["/api/__sitemap__/videos14"],
       },
-      'videos-15' : {
-        sources: [
-          '/api/__sitemap__/videos15',
-        ]
+      "videos-15": {
+        sources: ["/api/__sitemap__/videos15"],
       },
-      'videos-16' : {
-        sources: [
-          '/api/__sitemap__/videos16',
-        ]
+      "videos-16": {
+        sources: ["/api/__sitemap__/videos16"],
       },
-      'videos-17' : {
-        sources: [
-          '/api/__sitemap__/videos17',
-        ]
+      "videos-17": {
+        sources: ["/api/__sitemap__/videos17"],
       },
-      'videos-18' : {
-        sources: [
-          '/api/__sitemap__/videos18',
-        ]
+      "videos-18": {
+        sources: ["/api/__sitemap__/videos18"],
       },
-      'videos-19' : {
-        sources: [
-          '/api/__sitemap__/videos19',
-        ]
+      "videos-19": {
+        sources: ["/api/__sitemap__/videos19"],
       },
-      'videos-20' : {
-        sources: [
-          '/api/__sitemap__/videos20',
-        ]
+      "videos-20": {
+        sources: ["/api/__sitemap__/videos20"],
       },
-      'videos-21' : {
-        sources: [
-          '/api/__sitemap__/videos21',
-        ]
+      "videos-21": {
+        sources: ["/api/__sitemap__/videos21"],
       },
-      'videos-22' : {
-        sources: [
-          '/api/__sitemap__/videos22',
-        ]
+      "videos-22": {
+        sources: ["/api/__sitemap__/videos22"],
       },
-      'videos-23' : {
-        sources: [
-          '/api/__sitemap__/videos23',
-        ]
+      "videos-23": {
+        sources: ["/api/__sitemap__/videos23"],
       },
-      'videos-24' : {
-        sources: [
-          '/api/__sitemap__/videos24',
-        ]
+      "videos-24": {
+        sources: ["/api/__sitemap__/videos24"],
       },
-      'videos-25' : {
-        sources: [
-          '/api/__sitemap__/videos25',
-        ]
+      "videos-25": {
+        sources: ["/api/__sitemap__/videos25"],
       },
-      'videos-26' : {
-        sources: [
-          '/api/__sitemap__/videos26',
-        ]
+      "videos-26": {
+        sources: ["/api/__sitemap__/videos26"],
       },
-    }
+    },
   },
   i18n: {
     locales: [
